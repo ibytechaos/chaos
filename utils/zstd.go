a9641c2b9368f1e9168f752fc28d889026f86f75
wuzhipeng <wu.zhi.peng@outlook.com>
2023-05-01 23:10:39 +0800

    1. 修改zstd并发度

--- utils/zstd.go
+++ utils/zstd.go
@@ -11,13 +11,16 @@
 
 package utils
 
-import "github.com/klauspost/compress/zstd"
+import (
+	"github.com/klauspost/compress/zstd"
+	"runtime"
+)
 
 var (
 	// ZstdCompress
-	ZstdCompress, _ = zstd.NewWriter(nil)
+	ZstdCompress, _ = zstd.NewWriter(nil, zstd.WithEncoderConcurrency(runtime.NumCPU()))
 	// ZstdDecompress
-	ZstdDecompress, _ = zstd.NewReader(nil)
+	ZstdDecompress, _ = zstd.NewReader(nil, zstd.WithDecoderConcurrency(runtime.NumCPU()))
 )
 
 // Compress 压缩
